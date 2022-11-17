5179fde7b6edccf561860716028ef22d2aa40aea
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-11-16 22:00:07 -0300

    Create atividade11.js

---
+++ aula11/atividade11.js
@@ -0,0 +1,21 @@
+/*
+1) Refaça o exercício 5 da aula passada e os exercícios anteriores utilizando async e await.
+*/
+
+/*
+2) Usando async e await crie uma página que calcula o fibonati de um número mostrando o cálculo passo
+a passo.
+*/
+
+/*
+3. Usando programação assíncrona, crie uma webpage que realize um timer ou cronometro. 
+*/
+
+/*
+4. Usando programação assíncrona, crie uma webpage que realize um jogo de adivinhe o número, onde o 
+usuário entra com um número, e ele verifique se o número está certo ou não após 3 segundos. Depois 
+disso, após 5 segundos ele deixa o usuário tentar novamente.
+*/
+
+
+
