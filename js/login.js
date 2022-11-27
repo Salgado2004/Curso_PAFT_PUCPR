53e16e10d2985ee5a57acd7b4487769be2356d5b
Salgado2004 <leosalgado2004@gmail.com>
2022-11-27 18:15:48 -0300

    Validação de login

---
+++ js/login.js
@@ -0,0 +1,89 @@
+const info = document.querySelector("#info");
+
+info.oninput = function(e){
+    let identification = info.value;
+    let input = document.querySelector("#identification");
+    let label = document.querySelector("#idLabel");
+
+    label.innerHTML = `${identification}:`;
+    input.placeholder = identification;
+};
+
+
+const form = document.querySelector("#LoginForm");
+
+form.onsubmit = async function(e){
+    e.preventDefault();
+
+    let info = document.querySelector("#info").value;
+    let identification = document.querySelector("#identification").value;
+    let password = document.querySelector("#password").value;
+
+    try{
+        let response = await validateId(info, identification);
+        let pass = await validatePass(password);
+        let enter = await login(info, response, pass);
+        if (enter != null){
+            window.location.replace(enter);
+        }
+    } catch(error){
+        let warning = document.querySelector("#error");
+        warning.style.visibility = "visible";
+        warning.innerHTML = error;
+    }
+};
+
+function validateId(info, text){
+    switch(info){
+        case("CPF"):
+            let cpf = text.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+            if (/(\d{3}).(\d{3}).(\d{3})-(\d{2})/.test(cpf)){
+                return cpf;
+            } else{
+                throw "CPF inválido"
+            }
+        case("Nome"):
+            let nome = text;
+            if (/(\w)(\s)(\w)/.test(nome)){
+                return nome;
+            } else{
+                throw "Nome inválido"
+            }
+        case("Email"):
+            let email = text;
+            if (/(\w)@(\w).(\w)/.test(email)){
+                return email;
+            } else{
+                throw "Email inválido"
+            }
+    };
+};
+
+function validatePass(password){
+    if (/^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,15}$/.test(password)){
+        return password;
+    } else{
+        throw "Formato de senha inválido"
+    }
+}
+
+function login(info, id, pass){
+    let login;
+    switch(info){
+        case("CPF"):
+            login = id === "106.194.169-80";
+        break;
+        case("Email"):
+            login = id === "leosalgado2004@gmail.com";
+        break;
+        case("Nome"):
+            login = id === "Leonardo Salgado";
+        break;
+    }
+    let senha = pass === "Cr3@t1v3S3nh@";
+    if (login && senha){
+        return "home.html";
+    } else{
+        throw `${info} ou senha incorretos`
+    }
+}
