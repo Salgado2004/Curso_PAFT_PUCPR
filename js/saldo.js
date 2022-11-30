b68f1d8be9830647445b04ac546dc63cc2836938
Salgado2004 <leosalgado2004@gmail.com>
2022-11-30 14:20:52 -0300

    updates

--- js/saldo.js
+++ js/saldo.js
@@ -1,5 +1,8 @@
 document.body.onload = function(e){
     const valor = document.querySelector("#valor");
     let saldo = parseInt(localStorage.getItem("saldo"));
+    if (saldo == NaN){
+        saldo = 0;
+    }
     valor.innerHTML = `${saldo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
 }
