6f227d74b4c2d3f35432f4cd45fcaec50dd2dfd7
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-11-30 14:47:20 -0300

    Update saldo.js

--- js/saldo.js
+++ js/saldo.js
@@ -1,7 +1,3 @@
-window.localStorage.setItem("saldo", 0);
-console.log(localStorage.getItem("saldo"));
-console.log(parseInt(localStorage.getItem("saldo")));
-
 document.body.onload = function(e){
     const valor = document.querySelector("#valor");
     let saldo = parseInt(localStorage.getItem("saldo"));
