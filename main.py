8dbb517403e32e725c7486a48dff3b2deae87849
Salgado2004 <leosalgado2004@gmail.com>
2023-03-04 15:53:15 -0300

    Setup

--- main.py
+++ main.py
@@ -3,24 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-"""
-Usando o flask crie 3 rotas:
-
-1) Que consuma uma API e adapte o conteúdo para uma pagina web (crie tb uma pagina web que realize este consumo usando ajax/fetch)
-https://meowfacts.herokuapp.com/
-
-2) Que consuma duas APIs, combinando e adaptando o conteúdo para um json (crie tb uma pagina web que realize este consumo usando ajax/fetch)
-https://meowfacts.herokuapp.com/
-https://http.cat/
-
-3) Crie uma rota que retorne um template (JINJA) que consuma os dados de uma API (pode ser uma dar anteriores).
-https://meowfacts.herokuapp.com/
-https://placekitten.com/
- 
-Realize a verificação de erros nos retornos da API.
-"""
-
-
 @app.route('/')
 def index():
     resp = requests.get('https://meowfacts.herokuapp.com/?count=5&lang=por')
