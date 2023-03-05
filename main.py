86569c6d19d7c801360fbfd43e4ae71db3aa2b3d
Salgado2004 <leosalgado2004@gmail.com>
2023-03-04 21:02:11 -0300

    rota 1

--- main.py
+++ main.py
@@ -3,10 +3,35 @@ import requests
 
 app = Flask(__name__)
 
+@app.route("/rota1")
+def rota1():
+    return render_template('rota1.html')
+
+@app.route("/rota2/<movieId>")
+def rota2(movieId):
+    return render_template('rota2.html', id=movieId)
+
+@app.route("/getMovies")
+def getMovies():
+    params = {"api_key": "7128cc23bb835419dab02e5eab2923a5", "language": "pt-BR", "page": 10}
+    resp = requests.get('https://api.themoviedb.org/3/discover/movie', params=params)
+    movies = resp.json()
+    movieList = []
+    if movies['results'] != None:
+        for movie in movies['results']:
+            movieData = {
+                "cover": "https://image.tmdb.org/t/p/w500" + movie['poster_path'],
+                "title": movie['title'],
+                "resumo": movie['overview'],
+                "launch": movie['release_date'],
+                "nota": movie['vote_average'],
+                "id": movie['id']
+            }
+            movieList.append(movieData)
+        return movieList
+
 @app.route('/')
 def index():
-    resp = requests.get('https://meowfacts.herokuapp.com/?count=5&lang=por')
-    catFacts = resp.json()
-    return render_template('index.html', the_catfacts=catFacts)
+    return render_template('index.html')
 
 app.run(debug=True)
